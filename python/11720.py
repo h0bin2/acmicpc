1e34c58f7a0f2e4df5744a343f1acb96353ae434
ttpo3270 <ttpo3270@gmail.com>
2024-07-30 13:29:01 +0000

    240730 add

---
+++ python/11720.py
@@ -0,0 +1,7 @@
+N = int(input())
+S = input()
+output = 0
+for s in S:
+    output += int(s)
+
+print(output)
---
+++ python/9086.py
@@ -0,0 +1,4 @@
+T = int(input())
+for t in range(T):
+    S = input()
+    print(S[0] + S[-1])
