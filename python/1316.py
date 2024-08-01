6bf8de922b118f81cdfee2f9069a39479c094bfe
ttpo3270 <ttpo3270@gmail.com>
2024-08-01 14:54:39 +0000

    1316py done

--- python/1316.py
+++ python/1316.py
@@ -1,18 +1,20 @@
 N = int(input())
 cnt = 0
-for i in range(N):
-    check = []
+for n in range(N):
     word = input()
-    for j in range(len(word)):
-        try:
-            if word[j] != word[j+1]:
-                if word[j] in check:
-                    continue
-                else:
-                    check.append(word[j])
-                    cnt += 1
-        except:
-            if word[j] not in check:
-                check.append(word[j])
-                cnt += 1
+    check = ["" for i in range(len(word))]
+    wcnt = len(word)
+    for i in range(len(word)):
+        if i == 0:
+            check[i] = word[i]
+        else:
+            if word[i] != check[i-1] and word[i] in check:
+                break
+            else:
+                check[i] = word[i]
+                wcnt -= 1
+
+    if wcnt == 1:
+        cnt += 1    
+
 print(cnt)
